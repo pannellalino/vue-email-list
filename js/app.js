ef4c311fabcd925c7631e3e594ce0656887973a0
pannellalino <lista>
2022-11-11 16:43:21 +0100

    end

--- js/app.js
+++ js/app.js
@@ -8,11 +8,15 @@ createApp({
     }
   },
   methods:{
+    // prendo l Api url (funzione che mi genera randomicamente una mail) e lo stampo in pagina, uso un ciclo for per ottenere 10 mail random in pagina
     getApi(){
+    for(let i = 0; i < 10; i++){
       axios.get(this.apiUrl)
       .then(result =>{
-        this.email = result.data.response;
+        this.email.push(result.data.response);
+        console.log(result.data.response);
       })
+    }
     }
   },
   mounted(){
